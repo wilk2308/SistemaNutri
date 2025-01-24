7b86c39dc656e35eff901400b5345f30cbf7f0ca
Wiliam Gomes <williamdf2308@gmail.com>
2025-01-24 09:29:46 -0300

    dell

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,22 +1,14 @@
 <?php
 namespace App\Controllers;
 
-use App\Models\NoticiaModel;
-use App\Models\FeriasModel;
-use App\Models\EventModel; // Certifique-se de que existe um modelo para eventos
+
 
 class Home extends BaseController
 {
-    protected $noticiaModel;
-    protected $feriasModel;
-    protected $eventModel;
+   
 
-    public function __construct()
-    {
-        $this->noticiaModel = new NoticiaModel();
-        $this->feriasModel = new FeriasModel();
-        $this->eventModel = new EventModel(); // Inicialize o modelo de eventos
-    }
+
+ 
 
     public function index()
     {
@@ -24,21 +16,8 @@ class Home extends BaseController
             return redirect()->to('/login');
         }
 
-        $userData = session()->get('user');
-        $noticias = $this->noticiaModel->orderBy('data', 'DESC')->findAll();
-        $ferias = $this->feriasModel->orderBy('start_date', 'DESC')->findAll();
-
-        
-
-        // Buscar compromissos do dia atual
-        $today = date('Y-m-d');
-        $compromissosHoje = $this->eventModel->where('start', $today)->findAll();
-
         return view('home', [
-            'user' => $userData,
-            'noticias' => $noticias,
-            'ferias' => $ferias,
-            'compromissosHoje' => $compromissosHoje // Passar compromissos para a view
+     
         ]);
     }
 }
